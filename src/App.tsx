46739ed6698c917df1d4ec5f5d71efd032b0b036
Ekaterina Radchenko <60271557+cryingsealpup@users.noreply.github.com>
2024-05-15 16:00:52 +0400

    fix: add routing to login and register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import '../src/styles/index.sass';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './pages/about/About';
+import LoginPage from './views/LoginPage';
+import RegisterPage from './views/RegisterPage';
 import NotFoundPage from './pages/notFound/NotFound';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<About />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
         <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </Router>
--- src/constants/inputTypes.ts
+++
@@ -1,8 +0,0 @@
-enum InputType {
-  text = 'text',
-  number = 'number',
-  email = 'email',
-  password = 'password',
-  checkbox = 'checkbox',
-}
-export default InputType;
