95ed5c6257fac677da939d04d90100abe188b569
Ekaterina Radchenko <60271557+cryingsealpup@users.noreply.github.com>
2024-05-16 14:41:43 +0400

    feat: header component and page view with header

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import About from './pages/about/About';
 import LoginPage from './views/LoginPage';
 import RegisterPage from './views/RegisterPage';
 import NotFoundPage from './pages/notFound/NotFound';
-
+import Header from './components/Header';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<About />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-        <Route path="*" element={<NotFoundPage />}></Route>
-      </Routes>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<About />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
+        </Routes>
+      </main>
     </Router>
   );
 }
