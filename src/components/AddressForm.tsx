2b57eb30a35f204c4e7af87439d85edad7330678
Ekaterina Radchenko <60271557+cryingsealpup@users.noreply.github.com>
2024-05-21 17:35:42 +0400

    feat: address form

---
+++ src/components/AddressForm.tsx
@@ -0,0 +1,10 @@
+// import Button from './Button';
+// import Input from './Input';
+// import { ButtonType } from '../scripts/constants/enums';
+// export default function Address(onSubmit: () => void) {
+//   return (
+//     <form>
+//       <Button text="Save address" type={ButtonType.submit} />
+//     </form>
+//   );
+// }
---
+++ src/components/Select.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+
+interface SelectProps {
+  label: string | ReactNode;
+  options: Array<string>;
+  defaultOption: string;
+  name: string;
+  placeholder: string;
+  error?: boolean;
+  disabled?: boolean;
+  id?: 'number';
+  class?: string;
+}
+
+export default function Select({ ...props }: SelectProps) {
+  //   const [value, setValue] = useState('');
+  return (
+    <div>
+      <label htmlFor={props.name}> {props.label} </label>
+      <select
+        className={
+          props.class ||
+          'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        }
+        name={props.name}
+        disabled={props.disabled}
+        // value={value}
+        id={props.id}
+      >
+        <option hidden selected disabled>
+          {props.defaultOption}
+        </option>
+        {props.options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
