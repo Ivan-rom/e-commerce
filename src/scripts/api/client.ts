eeef1bcac48aeabbc2554e7929ca2f1c5100f80a
Ivan-rom <rom_1van@icloud.com>
2024-06-01 00:34:02 +0600

    feat: implement detailed product page with all current info

--- src/scripts/api/client.ts
+++ src/scripts/api/client.ts
@@ -74,6 +74,10 @@ export const getProduct = (id: string) => {
   return apiRoot.products().withId({ ID: id }).get().execute();
 };
 
+export const getCategory = (id: string) => {
+  return apiRoot.categories().withId({ ID: id }).get().execute();
+};
+
 // export const addToCard = (id: string) => {
 //   return apiRoot
 //     .carts()
