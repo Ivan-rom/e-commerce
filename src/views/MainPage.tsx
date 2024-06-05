dbd373beb442efc0a4750a7c08477a5e7930f2df
Ekaterina Radchenko <60271557+cryingsealpup@users.noreply.github.com>
2024-06-05 17:37:10 +0400

    feat: simple products display

--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -4,7 +4,7 @@ import About from '../pages/about/About';
 import { useEffect, useState } from 'react';
 import { getProducts } from '../scripts/api/client';
 // import { PageNames } from '../scripts/constants/enums';
-
+import getPrice from '../scripts/helpers/getPrice';
 // interfaces doesn't import for no reason
 // maybe something wrong with my pc
 // if it can work with ordinary imports use them instead
@@ -22,20 +22,34 @@ export default function Main() {
     <>
       <MotionConfig transition={{ duration: 1 }}>
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-          {/* <Header navPages={[PageNames.login, PageNames.register]} /> */}
           <About />
+          {/* temporary code to use product page */}
+          <div className="grid grid-cols-3 gap-3 m-auto w-9/12">
+            {products.map(
+              (product, index) =>
+                product.masterData.published && (
+                  <div key={index} className="w-full">
+                    <div>
+                      <a key={product.id} href={`product/${product.id}`}>
+                        <img
+                          className="h-60 object-cover m-auto"
+                          src={product.masterData.current.masterVariant.images![0].url}
+                          alt={`${product.masterData.current.name['en-US']} cover`}
+                        />
+                        <p className="italic text-xs">
+                          {getPrice(product.masterData.current.masterVariant)}$
+                        </p>
+
+                        <p className="font-bold">{product.masterData.current.name['en-US']}</p>
+                      </a>
+                    </div>
+                  </div>
+                ),
+            )}
+          </div>
+          {/* <Header navPages={[PageNames.login, PageNames.register]} /> */}
         </motion.div>
       </MotionConfig>
-
-      {/* temporary code to use product page */}
-      {products.map(
-        (product) =>
-          product.masterData.published && (
-            <a key={product.id} href={`product/${product.id}`}>
-              {product.masterData.current.name['en-US']},
-            </a>
-          ),
-      )}
     </>
   );
 }
